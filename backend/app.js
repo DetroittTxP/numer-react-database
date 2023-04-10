d4db933d1f88835592f2fe6912801f4e0a4ef455
Parame Srisomsap <s6304062616170@email.kmutnb.ac.th>
2023-04-10 22:11:12 +0700

    update swagger

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,116 @@ const cors = require('cors');
 const bodyparser = require('body-parser');
 const mysql = require('mysql2');
 const { compositeTrap } = require('./calculate/integration/Trapzoidal');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express')
+const swgfile = require('./swagger.json')
+
 
 
 const app = express();
 app.use(cors());
 app.use(bodyparser.json());
+
+
+const swaggerOptions = {
+    swaggerDefinition:{
+        info:{
+            title:'NUMER',
+            version: '1.0.0'
+        }
+    },
+    apis: ['./app.js'],
+};
+
+const swaggerSpec = swaggerJsDoc(swaggerOptions);
+
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+
+/**  
+* @swagger
+* /test:
+*   get:
+*     description: Get all name
+*     responses:
+*        200:
+*          description: SUCCESS
+*/      
+
+app.get('/test',(req,res)=>{
+    res.send([
+        {NAME:'HEHE',SIRNAME:'HAHA'}
+    ])
+})
+
+
+/**
+ * @swagger
+ * /randomtrap:
+ *   post:
+ *     summary: สุ่มสมการ
+ *     responses:
+ *       200:
+ *         description: เยี่ยมมากกกกกกกกกกกกกกก
+ *     tags:
+ *       - randomtrap
+ */
+
+app.post('/randomtrap',(req,res)=>{
+
+    db.query(`SELECT * FROM intregrate ORDER BY RAND() LIMIT 1;`,(error, results, fields)=>{
+        if(error){
+            console.log('hehe');
+        }   
+        res.json(results);
+    })
+})
+
+
+
+/**
+ * @swagger
+ * /caltrap:
+ *   post:
+ *     summary: "คำนวณ trap"
+ *     consumes:
+ *      - "application/json"
+ *     produces:
+ *      - "application/json"
+ *     parameters:
+ *      - in: body
+ *        name: equation 
+ *        description: "สมการ"
+ *        required: true
+ *        schema:
+ *          type: object
+ *          properties:
+ *             equation:
+ *                type: string
+ *             xlower:
+ *                type: number
+ *             xupper:
+ *                type: number
+ *             n:
+ *                type: number
+ *     responses:
+ *        200:
+ *          description: "The result of the integral calculation"
+ * 
+ * 
+ */
+
+app.post('/caltrap',(req,res)=>{
+    const {equation,xlower,xupper,n} = req.body
+    res.json(compositeTrap(equation,xlower,xupper,n))
+})
+
+
+
+
+
+
+
+
 
 const db = mysql.createConnection({
   host: 'localhost',
@@ -39,15 +144,7 @@ app.post('/caltrap',(req,res)=>{
     res.json(compositeTrap(equation,xlower,xupper,n))
 })
 
-app.post('/randomtrap',(req,res)=>{
 
-    db.query(`SELECT * FROM intregrate ORDER BY RAND() LIMIT 1;`,(error, results, fields)=>{
-        if(error){
-            console.log('hehe');
-        }   
-        res.json(results);
-    })
-})
 
 
 
